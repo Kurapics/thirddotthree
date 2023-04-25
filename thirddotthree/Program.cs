e148db32dca4d279bae73b2b1cb111452521d201
Компьютер <@DESKTOP-0RODP2R>
2023-04-25 22:23:36 +0200

    Добавьте файлы проекта.

---
+++ thirddotthree/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите x: ");
+        double x = double.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("Введите кол-во итераций");
+        int iters = int.Parse(Console.ReadLine());
+        double res = 0;
+        if (iters > 0) res += Math.Sin(x);
+        for (int i = 1; i < iters; i++)
+        {
+            int temp = 2;
+            res += (Math.Cos(temp * x) / temp);
+        }
+        Console.WriteLine(res);
+    }
+}
